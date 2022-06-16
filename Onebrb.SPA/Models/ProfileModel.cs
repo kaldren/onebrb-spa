c4fca5f617dd1c9342bddaf81b4bd5aba76a3fce
Kaloyan Drenski <drenski666@gmail.com>
2022-06-16 17:51:12 +0300

    Added ProfileModel and Profile component

---
+++ Onebrb.SPA/Models/ProfileModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Onebrb.SPA.Models;
+
+public class ProfileModel
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public string About { get; set; } = string.Empty;
+
+    public bool IsDeleted { get; set; }
+}
+

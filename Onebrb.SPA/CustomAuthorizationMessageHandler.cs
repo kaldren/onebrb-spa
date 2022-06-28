8195792880248d900d0aaead81025534645180d9
Kaloyan Drenski <drenski666@gmail.com>
2022-06-28 11:57:14 +0300

    WORKING AD B2C WITH API!!!

---
+++ Onebrb.SPA/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+{
+    public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+        NavigationManager navigationManager)
+        : base(provider, navigationManager)
+    {
+        //ConfigureHandler(
+        //    authorizedUrls: new[] { "https://localhost:7088/api" },
+        //    scopes: new[] { "https://onebrb.onmicrosoft.com/api/Onebrb.Read" });
+
+
+        ConfigureHandler(new[] { "https://localhost:7088/api" });
+    }
+}

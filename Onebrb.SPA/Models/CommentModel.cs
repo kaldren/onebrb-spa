da78a88f3e1bd347c146b9cad79552a56ad67a8e
Kaloyan Drenski <drenski666@gmail.com>
2022-07-04 16:13:50 +0300

    Fetch comments from API

---
+++ Onebrb.SPA/Models/CommentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Onebrb.SPA.Models
+{
+    public class CommentModel
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhotoUrl { get; set; } = "https://placekitten.com/200/200";
+    }
+}

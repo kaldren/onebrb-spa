52c07aebccc57918aed4bc9743e9c9b1d20a6978
Kaloyan Drenski <drenski666@gmail.com>
2022-07-05 13:47:56 +0300

    Added properties

--- Onebrb.SPA/Models/CommentModel.cs
+++ Onebrb.SPA/Models/CommentModel.cs
@@ -3,10 +3,19 @@
     public class CommentModel
     {
         public int CommentId { get; set; }
+
         public string Content { get; set; }
+
+        public long AuthorId { get; set; }
+
         public string AuthorFirstName { get; set; }
+
         public string AuthorLastName { get; set; }
+
         public string PhotoUrl { get; set; } = "https://placekitten.com/200/200";
+
         public short StarRating { get; set; } = 5;
+
+        public DateTimeOffset Date { get; set; }
     }
 }

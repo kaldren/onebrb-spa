ed8ffd29eae45ced026be6cf446d26a6cdc073a5
Kaloyan Drenski <drenski666@gmail.com>
2022-07-05 18:32:46 +0300

    Added JsonSerializerOptions for camelCase => PascalCase

--- Onebrb.SPA/Models/ProfileModel.cs
+++ Onebrb.SPA/Models/ProfileModel.cs
@@ -4,15 +4,15 @@ public class ProfileModel
 {
     public long Id { get; set; }
 
-    public string FirstName { get; set; } = string.Empty;
+    public string FirstName { get; set; }
 
-    public string LastName { get; set; } = string.Empty;
+    public string LastName { get; set; }
 
-    public string Email { get; set; } = string.Empty;
+    public string Email { get; set; }
 
-    public string Phone { get; set; } = string.Empty;
+    public string Phone { get; set; }
 
-    public string About { get; set; } = string.Empty;
+    public string About { get; set; }
 
     public bool IsDeleted { get; set; }
 }
